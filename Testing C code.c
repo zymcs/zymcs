1b79f7b29af994cb0b15f99fb75d56762400b4a9
zymcs <jawad12jawad@outlook.com>
2024-10-27 21:41:55 +0300

    Testing out my code
    I added X Y Z and made a condition that if the input is 25 then it's correct. else it's wrong.

--- Testing C code.c
+++ Testing C code.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!");
+    int x = 5;
+    int y = x
+    int z = y*x
+    printf("What would the answer be?");
+    scanf("input = %d")
+if input == 25;
+printf("you're correct!")
+
+else if
+printf("You're wrong!")
   return 0;
 }
